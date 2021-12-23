a1027b39af06eb6b78fd8ae3abd66b1966d02654
Dominik Brüchner <dominik.br@posteo.net>
2021-12-23 21:39:50 +0100

    add menu toggle

--- components/HeaderMenu.vue
+++ components/HeaderMenu.vue
@@ -1,5 +1,12 @@
 <template>
 	<v-app-bar fixed app>
+		<v-btn
+			v-if="$vuetify.breakpoint.smAndDown"
+			icon
+			@click.stop="handleToggleOpen"
+		>
+			<v-icon>mdi-menu</v-icon>
+		</v-btn>
 		<v-img
 			class="mx-2"
 			:src="require('~/static/images/opeb_logo.gif')"
@@ -12,13 +19,34 @@
 			<v-btn text> Tools & Stats </v-btn>
 			<v-btn text> About </v-btn>
 		</div>
-		<v-spacer />
-		<v-btn depressed color="primary"> Login </v-btn>
+		<v-spacer v-if="$vuetify.breakpoint.mdAndUp" />
+		<v-btn
+			v-if="$vuetify.breakpoint.mdAndUp"
+			depressed
+			outlined
+			color="primary"
+		>
+			<v-icon left>mdi-open-in-new</v-icon> Benchmark your Tool
+		</v-btn>
+		<v-btn v-if="$vuetify.breakpoint.mdAndUp" depressed color="ml-3 primary">
+			<v-icon left>mdi-login-variant</v-icon> Login
+		</v-btn>
 	</v-app-bar>
 </template>
 
 <script>
 export default {
 	name: 'HeaderMenu',
+	data() {
+		return {
+			openNavMobile: null,
+			fixed: false,
+		};
+	},
+	methods: {
+		handleToggleOpen() {
+			this.openNavMobile = !this.openNavMobile;
+		},
+	},
 };
 </script>
