358addf6a8c199e952edc745a412a68eeaa7c19f
Dominik Brüchner <dominik.br@posteo.net>
2022-02-02 13:06:45 +0100

    enabled nested routes for communities and participants

--- pages/scientific/_community/_id.spec.js
+++ pages/scientific/_community/_id.spec.js
@@ -1,13 +1,17 @@
 import { mount } from '@vue/test-utils';
-import Communities from './scientific.vue';
+import Community from './_id.vue';
 
 const factory = () => {
-	return mount(Communities, {
+	return mount(Community, {
 		...createComponentMocks({}),
+		mocks: {
+			$route: { params: { community: 'TESTCOMMUNITY', id: 'TESTID' } },
+			$config: { OEB_LEGACY_ANGULAR_URI: 'https://jest-openebench.bsc.es/' },
+		},
 	});
 };
 
-describe('Communities', () => {
+describe('Community Participant', () => {
 	it('is instantiated', () => {
 		const wrapper = factory();
 		expect(wrapper).toBeTruthy();
@@ -17,7 +21,7 @@ describe('Communities', () => {
 		const wrapper = factory();
 		expect(wrapper.vm.hostName).toBe('https://jest-openebench.bsc.es/');
 		expect(wrapper.find('iframe').attributes('src')).toBe(
-			'https://jest-openebench.bsc.es/scientific'
+			'https://jest-openebench.bsc.es/scientific/TESTCOMMUNITY/TESTID'
 		);
 	});
 });
