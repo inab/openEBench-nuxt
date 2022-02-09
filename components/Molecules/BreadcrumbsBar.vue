459d9f63e9865a72f11ab02330e039bce15d22d0
Dominik Brüchner <dominik.br@posteo.net>
2022-02-09 18:49:32 +0100

    first implementation

---
+++ components/Molecules/BreadcrumbsBar.vue
@@ -0,0 +1,9 @@
+<template>
+	<v-breadcrumbs v-bind="$attrs" class="hidden-sm-and-down">
+		<template #divider> <v-icon>mdi-chevron-right</v-icon> </template>
+	</v-breadcrumbs>
+</template>
+
+<script>
+export default {};
+</script>
--- pages/scientific/_community/index.vue
+++ pages/scientific/_community/index.vue
@@ -1,21 +1,47 @@
 <template>
-	<iframe
-		:src="hostName + `scientific/` + $route.params.community"
-		width="100%"
-		height="100%"
-		frameborder="0"
-		class="mt-5"
-	>
-	</iframe>
+	<v-container fluid class="fill-height fill-width">
+		<breadcrumbs-bar :items="breadcrumbs" />
+		<iframe
+			:src="hostName + `scientific/` + $route.params.community"
+			width="100%"
+			height="100%"
+			frameborder="0"
+			class="mt-5"
+		/>
+	</v-container>
 </template>
 
 <script>
+import BreadcrumbsBar from '~/components/Molecules/BreadcrumbsBar';
+
 export default {
 	name: 'CommunityPage',
+	components: {
+		BreadcrumbsBar,
+	},
 	layout: 'embedIframeFullWidth',
 	data() {
 		return {
 			hostName: this.$config.OEB_LEGACY_ANGULAR_URI,
+			breadcrumbs: [
+				{
+					text: 'Dashboard',
+					disabled: false,
+					exact: true,
+					to: '/',
+				},
+				{
+					text: 'Benchmarking Communities',
+					disabled: false,
+					exact: true,
+					to: '/scientific',
+				},
+				{
+					text: this.$route.params.community,
+					disabled: true,
+					to: this.$route.params.community,
+				},
+			],
 		};
 	},
 };
