53a68d70d33022f196a3601e2118fcd8462be85f
Dominik Brüchner <dominik.br@posteo.net>
2022-02-10 09:38:43 +0100

    some adjustments to landing page

---
+++ components/Cards/FeatureCard.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<v-avatar color="primary" size="62" class="mb-10">
+			<slot name="icon" />
+		</v-avatar>
+		<h2 class="text-h6 mb-2"><slot name="title" /></h2>
+		<p class="body-1">
+			<slot name="description" />
+		</p>
+		<v-btn class="mt-2" text color="primary" :to="to"> Learn More </v-btn>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'FeatureCard',
+	props: {
+		to: {
+			type: String,
+			required: true,
+		},
+	},
+};
+</script>
