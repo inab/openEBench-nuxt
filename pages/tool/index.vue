a0ed115fcb7f8fa3da213ec083b6f4741bc50d6c
emarti2 <eva.martin@bsc.es>
2023-07-14 12:57:59 +0200

    feat: breadcrumbs items style customizable

--- pages/tool/index.vue
+++ pages/tool/index.vue
@@ -1,21 +1,36 @@
 <template>
-	<iframe
-		:src="hostName + `tool?search=` + query"
-		width="100%"
-		height="100%"
-		frameborder="0"
-	>
-	</iframe>
+	<v-container fluid class="pa-0 ma-0">
+		<breadcrumbs-bar
+			v-if="breadcrumbs.length > 0"
+			:items="breadcrumbs"
+			dark
+			class-disabled="{ white--text }"
+			class-active="{ white--text text-decoration-underline }"
+		/>
+		<MainCard />
+		<v-row>
+			<v-col justify="center" cols="7" class="mx-auto">
+				<SearchCarousel class="mt-2" />
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
 <script>
+import BreadcrumbsBar from '~/components/Molecules/BreadcrumbsBar';
+import MainCard from '~/components/Tools/Search/MainCard.vue';
+import SearchCarousel from '~/components/Tools/Search/SearchCarousel.vue';
+
 export default {
 	name: 'ToolsMonitoringPage',
-	layout: 'embedIframeFullWidth',
+	components: {
+		BreadcrumbsBar,
+		MainCard,
+		SearchCarousel,
+	},
+	layout: 'mainWOBreadcrumbs',
 	data() {
 		return {
-			hostName: this.$config.OEB_LEGACY_ANGULAR_URI,
-			query: this.$route.query.search ? this.$route.query.search : '',
 			breadcrumbs: [
 				{
 					text: 'Home',
@@ -30,8 +45,23 @@ export default {
 			],
 		};
 	},
+	watch: {
+		$route(newRoute, oldRoute) {
+			if (newRoute.path !== oldRoute.path) this.breadcrumbs = [];
+		},
+	},
 	mounted() {
 		this.$parent.$emit('emitBreadcrumbs', this.breadcrumbs);
 	},
 };
 </script>
+<style scoped>
+.v-breadcrumbs {
+	background-color: #0b579f !important;
+	color: white !important;
+}
+
+.v-breadcrumbs >>> v-breadcrumbs__item {
+	color: white !important;
+}
+</style>
