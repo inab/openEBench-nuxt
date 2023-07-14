94f3f2a03c0735a8b9503579bf1f1f48b9d81fc4
emarti2 <eva.martin@bsc.es>
2023-07-14 12:57:59 +0200

    feat: breadcrumbs items style customizable

--- components/Molecules/BreadcrumbsBar.vue
+++ components/Molecules/BreadcrumbsBar.vue
@@ -1,5 +1,44 @@
 <template>
-	<v-breadcrumbs v-bind="$attrs" class="hidden-xs-only">
+	<v-breadcrumbs v-bind="$attrs" class="hidden-xs-only" :items="$attrs.items">
 		<template #divider> <v-icon>mdi-chevron-right</v-icon> </template>
+		<!--The following slot allow to customize the class of items-->
+		<!--The classes are passed as props (classDisabled, classActive)-->
+		<template #item="{ item }">
+			<v-breadcrumbs-item
+				v-if="item.disabled"
+				:class="colorClass"
+				:href="item.to"
+				:disabled="item.disabled"
+			>
+				<span :class="classDisabled">{{ item.text }}</span>
+			</v-breadcrumbs-item>
+			<v-breadcrumbs-item
+				v-else
+				:class="colorClass"
+				:href="item.to"
+				:disabled="item.disabled"
+			>
+				<span :class="classActive">{{ item.text }}</span>
+			</v-breadcrumbs-item>
+		</template>
 	</v-breadcrumbs>
 </template>
+<script>
+export default {
+	name: 'BreadcrumbsBar',
+	props: {
+		classDisabled: {
+			type: String,
+			default: '',
+			required: false,
+			// example: '{ white--text }',
+		},
+		classActive: {
+			type: String,
+			default: '',
+			required: false,
+			// example: '{ white--text text-decoration-underline }',
+		},
+	},
+};
+</script>
