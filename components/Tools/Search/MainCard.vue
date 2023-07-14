6142f92436a737d2017a51605e7d25add36c8725
emarti2 <eva.martin@bsc.es>
2023-07-14 13:17:26 +0200

    feat: components of tool and mainwobreadcreumbs layout added

---
+++ components/Tools/Search/MainCard.vue
@@ -0,0 +1,62 @@
+<template>
+	<v-row id="search-container" align="center" justify="center" class="mr-0">
+		<v-col cols="10" class="text-center mt-4 mb-0 pb-0">
+			<h3 class="text-h5 mt-8">Explore Tools in OpenEBench</h3>
+		</v-col>
+		<v-col cols="5" class="mt-0 mb-5 pt-0">
+			<v-text-field v-model="value" solo hide-details class="mb-2">
+				<template #append-outer>
+					<v-btn elevation="2" color="#021A54" dark> Search </v-btn>
+				</template>
+			</v-text-field>
+			<span class="mt-4 pt-4 pl-2 text-body-2 white--text">
+				Examples:
+				<span v-for="(item, i) in exampleValues" :key="i">
+					<b
+						><a class="white--text" @click="inputExample(item.name)">
+							{{ item.name }}
+						</a></b
+					>
+					<span v-if="i < exampleValues.length - 1">, </span>
+				</span>
+			</span>
+		</v-col>
+	</v-row>
+</template>
+<script>
+export default {
+	name: 'MainCard',
+	data() {
+		return {
+			value: '',
+			exampleValues: [
+				{
+					name: 'trimAl',
+				},
+				{
+					name: 'PyMut',
+				},
+			],
+		};
+	},
+	methods: {
+		inputExample(value) {
+			this.value = value;
+		},
+	},
+};
+</script>
+<style scoped>
+#search-container {
+	background-color: #0b579f !important;
+	color: white;
+	top: 0;
+	left: 0;
+	right: 0;
+	height: 300px;
+}
+
+.v-input >>> v-input__control {
+	margin-top: 4px !important;
+}
+</style>
---
+++ components/Tools/Search/SearchCarousel.vue
@@ -0,0 +1,30 @@
+<template>
+	<v-carousel cycle height="500" hide-delimiter-background show-arrows-on-hover>
+		<v-carousel-item v-for="(slide, i) in slides" :key="i">
+			<v-sheet color="gery lighten-3" height="100%">
+				<v-row class="fill-height" align="center" justify="center">
+					<div class="text-h5">
+						{{ slide }}
+					</div>
+				</v-row>
+			</v-sheet>
+		</v-carousel-item>
+	</v-carousel>
+</template>
+
+<script>
+export default {
+	name: 'SearchCarousel',
+	data() {
+		return {
+			slides: [
+				'First - sample content 🚧',
+				'Second - sample content 🚧',
+				'Third - sample content 🚧',
+				'Fourth - sample content 🚧',
+				'Fifth - sample content 🚧',
+			],
+		};
+	},
+};
+</script>
