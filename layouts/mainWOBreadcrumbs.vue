56dd077f1e8e795c68bc236b2356172a7a443dc0
emarti2 <eva.martin@bsc.es>
2023-07-14 13:17:26 +0200

    feat: components of tool and mainwobreadcreumbs layout added

---
+++ layouts/mainWOBreadcrumbs.vue
@@ -0,0 +1,37 @@
+<template>
+	<v-app>
+		<main-header :vre-href="$config.VRE_URI" />
+		<v-main>
+			<Nuxt />
+			<Footer class="mt-16" />
+		</v-main>
+		<v-footer class="justify-center" app>
+			Made with <v-icon color="red">mdi-heart</v-icon>
+			in Barcelona.
+		</v-footer>
+	</v-app>
+</template>
+
+<script>
+import HeaderMenu from '~/components/Header/HeaderMenu.vue';
+import Footer from '~/components/TheFooter';
+
+export default {
+	name: 'DefaultLayout',
+	components: {
+		'main-header': HeaderMenu,
+		Footer,
+	},
+	data() {
+		return {
+			breadcrumbs: [],
+		};
+	},
+	head() {
+		return {
+			title:
+				'OpenEBench - The ELIXIR gateway to benchmarking communities, software monitoring, and quality metrics',
+		};
+	},
+};
+</script>
