079e03ea1659da94ecf0d0b71d3786318d43701e
Eva Martin del Pico <eva.mdpico@gmail.com>
2023-12-05 12:10:24 +0100

    feat(license-content): add alert for when license is not available

---
+++ components/Tools/ToolEntry/License/LicenseContent.vue
@@ -0,0 +1,28 @@
+<template>
+	<v-container class="mt-0 pt-0 pl-8">
+		<v-row v-if="tool.license" class="mt-0 pt-0 mb-2"> Hello </v-row>
+		<v-row v-else justify="left" class="mt-0 pt-0 mb-2">
+			<v-alert
+				dense
+				icon="mdi-scale-balance"
+				color="grey"
+				text
+				class="ml-5 mt-3"
+			>
+				License information not available
+			</v-alert>
+		</v-row>
+	</v-container>
+</template>
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+	name: 'LicenseContent',
+	computed: {
+		...mapGetters('tool_entry', {
+			tool: 'tool',
+		}),
+	},
+};
+</script>
