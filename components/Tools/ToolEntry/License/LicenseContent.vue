43cee2c9287acc7b3444e9addc42d5e190da86b2
Eva Martin del Pico <eva.mdpico@gmail.com>
2024-03-22 10:24:30 +0100

    fix:

--- components/Tools/ToolEntry/License/LicenseContent.vue
+++ components/Tools/ToolEntry/License/LicenseContent.vue
@@ -2,12 +2,12 @@
 	<v-container class="mt-0 pt-0 pl-8">
 		<div v-if="tool.license" class="mt-2 pt-0 mb-2 ml-5">
 			<!--License Cards-->
-			<LicenseRow
+			<!--LicenseRow
 				v-for="item in tool.license"
 				:key="item.id"
 				:name="item.term.name"
 				:url="item.term.url"
-			/>
+			/-->
 		</div>
 		<v-row v-else justify="left" class="mt-0 pt-0 mb-2">
 			<v-alert
@@ -24,13 +24,9 @@
 </template>
 <script>
 import { mapGetters } from 'vuex';
-import LicenseRow from './LicenseRow.vue';
 
 export default {
 	name: 'LicenseContent',
-	components: {
-		LicenseRow,
-	},
 	computed: {
 		...mapGetters('tool_entry', {
 			tool: 'tool',
