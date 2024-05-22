538066150b82dde4803c6701e812b96a2f2bc8ba
Eva Martin del Pico <eva.mdpico@gmail.com>
2024-05-23 01:18:11 +0200

    feat: add files

---
+++ components/observatory/evaluation/Results/MetadataActions.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<ViewMetadata />
+		<ExportMetadataCardDownload />
+		<ExportMetadataCardPull />
+	</div>
+</template>
+<script>
+import ExportMetadataCardDownload from '~/components/observatory/evaluation/Export/ExportMetadataCardDownload.vue';
+import ExportMetadataCardPull from '~/components/observatory/evaluation/Export/ExportMetadataCardPull.vue';
+import ViewMetadata from '~/components/observatory/evaluation/Export/ViewMetadata.vue';
+
+export default {
+	name: 'MetadataActions',
+	components: {
+		ExportMetadataCardDownload,
+		ExportMetadataCardPull,
+		ViewMetadata,
+	},
+};
+</script>
