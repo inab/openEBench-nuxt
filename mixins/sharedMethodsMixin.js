868d36f8f306107c733badb6f726ba9fc3257804
Andrea <andrea.morales@bsc.es>
2024-06-06 12:21:08 +0200

    Added shared css and functions

---
+++ mixins/sharedMethodsMixin.js
@@ -0,0 +1,26 @@
+// mixins/sharedMethodsMixin.js
+export default {
+	methods: {
+		getItemColor(role) {
+			const item = this.items.find((item) => item.word === role);
+			return item ? item.color : '#ffffff';
+		},
+		getItemIcon(role) {
+			const item = this.items.find((item) => item.word === role);
+			return item ? `url(${item.icon})` : '';
+		},
+		formatRoleName(name) {
+			// Check if the role name contains parentheses
+			const index = name.indexOf('(');
+			if (index !== -1) {
+				const firstPart = name.substring(0, index).trim();
+				const secondPart = name.substring(index).trim();
+				return `${firstPart}\n${secondPart}`;
+			}
+			return name;
+		},
+		toggleRole(role) {
+			role.hover = !role.hover;
+		},
+	},
+};
