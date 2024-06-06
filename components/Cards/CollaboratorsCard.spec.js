3ab888dda9d3e24f3a96c48c43fe5998bcc3eadf
avalle <ani.valle@bsc.es>
2024-06-06 14:46:41 +0200

    Add collaboration.spec

---
+++ components/Cards/CollaboratorsCard.spec.js
@@ -0,0 +1,28 @@
+import { shallowMount } from '@vue/test-utils';
+import CollaboratorsCard from './CollaboratorsCard.vue';
+
+const factory = (propsData) => {
+	return shallowMount(CollaboratorsCard, {
+		mocks: {
+			$vuetify: { breakpoint: {} },
+		},
+		propsData: {
+			title: 'Project Name',
+			href: 'https://projectname.com',
+			src: 'https://via.placeholder.com/150',
+			...propsData
+		}
+	});
+};
+
+describe('CollaboratorsCard', () => {
+	it('is instantiated', () => {
+		const wrapper = factory();
+		expect(wrapper).toBeTruthy();
+	});
+
+	it('should match snapshot', () => {
+		const wrapper = factory();
+		expect(wrapper.html()).toMatchSnapshot();
+	});
+});
--- pages/collaboration.spec.js
+++ pages/collaboration.spec.js
@@ -4,6 +4,14 @@ import About from './collaboration.vue';
 const factory = () => {
 	return mount(About, {
 		...createComponentMocks({}),
+		stubs: {
+			'v-container': true,
+			'v-row': true,
+			'v-col': true,
+			'v-img': true,
+			'v-card': true,
+			'v-divider': true,
+		},
 	});
 };
 
@@ -12,4 +20,19 @@ describe('Collaboration', () => {
 		const wrapper = factory();
 		expect(wrapper).toBeTruthy();
 	});
+	it('should match snapshot', () => {
+		const wrapper = factory();
+		expect(wrapper.html()).toMatchSnapshot();
+	});
+	it('passes the correct props to CollaboratorsCard components', () => {
+		const wrapper = factory();
+		const collaborators = wrapper.vm.collaborators;
+		const cards = wrapper.findAllComponents(CollaboratorsCard);
+		cards.wrappers.forEach((card, index) => {
+			expect(card.props().title).toBe(collaborators[index].title);
+			expect(card.props().href).toBe(collaborators[index].href);
+			expect(card.props().src).toBe(collaborators[index].src);
+		});
+	});
+
 });
