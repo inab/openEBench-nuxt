d7db02476ef1394d0890ae17ce37d5df7ee28df4
Andrea <andrea.morales@bsc.es>
2024-07-04 16:49:54 +0200

    Update teams files and functions

--- mixins/sharedMethodsMixin.js
+++ mixins/sharedMethodsMixin.js
@@ -1,27 +1,6 @@
 // mixins/sharedMethodsMixin.js
 export default {
 	methods: {
-		getItemColor(role) {
-			const item = this.items.find((item) => item.word === role);
-			return item ? item.color : '#ffffff';
-		},
-		getItemIcon(role) {
-			const item = this.items.find((item) => item.word === role);
-			return item ? `url(${item.icon})` : '';
-		},
-		formatRoleName(name) {
-			// Check if the role name contains parentheses
-			const index = name.indexOf('(');
-			if (index !== -1) {
-				const firstPart = name.substring(0, index).trim();
-				const secondPart = name.substring(index).trim();
-				return `${firstPart}\n${secondPart}`;
-			}
-			return name;
-		},
-		toggleRole(role) {
-			role.hover = !role.hover;
-		},
 		formatDate(dateString) {
 			const options = { year: 'numeric', month: 'long', day: 'numeric' };
 			return new Date(dateString).toLocaleDateString('en-US', options);
