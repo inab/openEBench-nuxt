487fc71d026aade0f2500d9e686afc11f32795e3
Andrea <andrea.morales@bsc.es>
2024-07-08 17:57:07 +0200

    Added a space between the number of challenges and the word

--- components/Communities/CommunityEventSelector.vue
+++ components/Communities/CommunityEventSelector.vue
@@ -47,7 +47,7 @@
 			<!-- Challenges count for desktop (conditionally hidden) -->
 			<div class="d-flex align-center" v-if="!isMobile">
 				<v-icon class="mr-1 text--primary">mdi-flag-outline</v-icon>
-				<span>{{ currentEvent.challenges.length }}</span>
+				<span>{{ currentEvent.challenges.length }}&nbsp;</span>
 				<span>{{
 					'Challenges' | pluralize(currentEvent.challenges.length)
 				}}</span>
