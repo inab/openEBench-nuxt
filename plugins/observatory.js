c1a0c8991ae688a8f852d12327f33cf9fdaca834
Eva Martin del Pico <eva.mdpico@gmail.com>
2024-07-24 19:39:00 +0200

    fix: hardcode URLs in observatory and githubapp plugins

--- plugins/observatory.js
+++ plugins/observatory.js
@@ -1,4 +1,7 @@
-export default function ({ $axios, $config: { OBSERVATORY_API_URL } }, inject) {
+export default function (
+	{ $axios, $config: { _OBSERVATORY_API_URL } },
+	inject
+) {
 	// Create a custom axios instance
 	const observatory = $axios.create({
 		headers: {
@@ -9,8 +12,8 @@ export default function ({ $axios, $config: { OBSERVATORY_API_URL } }, inject) {
 	});
 
 	// Set baseURL to something different
-	observatory.setBaseURL(OBSERVATORY_API_URL);
-	// observatory.setBaseURL('http://127.0.0.1:3500');
+	// observatory.setBaseURL(OBSERVATORY_API_URL);
+	observatory.setBaseURL('https://dev-observatory.openebench.bsc.es/api');
 
 	// Inject to context as $api
 	inject('observatory', observatory);
