317afd001d8085db6ecb2dc18db2e1831c0af44c
Jessica F. Martinez <jessicafndez@gmail.com>
2024-08-21 16:39:44 +0200

    FIX logout

--- plugins/keycloak.js
+++ plugins/keycloak.js
@@ -15,7 +15,8 @@ export default class KeycloakScheme extends Oauth2Scheme {
 	logout() {
 		if (this.options.endpoints.logout) {
 			const opts = {
-				redirect_uri: this.logoutRedirectURI,
+				client_id: this.options.clientId,
+				post_logout_redirect_uri: this.logoutRedirectURI,
 			};
 			const url = this.options.endpoints.logout + '?' + encodeQuery(opts);
 			window.location.replace(url);
