667ee670284066dca61db1a87458bf98c5c7bcb1
Eva Martin del Pico <eva.mdpico@gmail.com>
2024-10-24 15:09:56 +0200

    fix: plugin URLs

--- plugins/observatory.js
+++ plugins/observatory.js
@@ -1,7 +1,4 @@
-export default function (
-	{ $axios, $config: { _OBSERVATORY_API_URL } },
-	inject
-) {
+export default function ({ $axios, $config: { OBSERVATORY_API_URL } }, inject) {
 	// Create a custom axios instance
 	const observatory = $axios.create({
 		headers: {
@@ -12,9 +9,9 @@ export default function (
 	});
 
 	// Set baseURL to something different
-	observatory.setBaseURL('http://0.0.0.0:3500');
+	// observatory.setBaseURL('http://0.0.0.0:3500');
 
-	// observatory.setBaseURL(OBSERVATORY_API_URL);
+	observatory.setBaseURL(OBSERVATORY_API_URL);
 
 	// Inject to context as $api
 	inject('observatory', observatory);
