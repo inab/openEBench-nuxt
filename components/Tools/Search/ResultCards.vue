dda2d7ed45404b992e9feac5c34f4775aa60abd3
Eva Martin del Pico <eva.mdpico@gmail.com>
2026-02-26 14:13:43 +0100

    fixing search

--- components/Tools/Search/ResultCards.vue
+++ components/Tools/Search/ResultCards.vue
@@ -30,10 +30,10 @@
 					:publications="tool.publication"
 					:license="tool.license"
 					:webpage="tool.webpage[0]"
-					:findability="tool.indicators_scores.F"
-					:accessibility="tool.indicators_scores.A"
-					:interoperability="tool.indicators_scores.I"
-					:reusability="tool.indicators_scores.R"
+					findability="1"
+					accessibility="1"
+					interoperability="1"
+					reusability="1"
 					:order="i"
 				/>
 			</v-col>
