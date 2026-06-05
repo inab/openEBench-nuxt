6596ea4bce4ea1954a7ff7bbbe2082bbeead52bf
Andrea <andrea.morales@bsc.es>
2026-06-05 15:45:47 +0200

    Homoginize the v-skeleton, less width

--- pages/tool/index.vue
+++ pages/tool/index.vue
@@ -9,13 +9,7 @@
 					</div>
 				</v-col>
 				<v-col cols="12" md="8" lg="9" xl="9">
-					<v-row v-if="loading.initialSearch" justify="center" class="mt-5">
-						<v-col v-for="n in 9" :key="n" cols="12">
-							<v-skeleton-loader v-bind="attrs" type="article" />
-						</v-col>
-					</v-row>
-
-					<v-row v-else ref="scrollBox" class="mt-1">
+					<v-row ref="scrollBox" class="mt-1">
 						<v-col cols="12">
 							<ResultCards />
 						</v-col>
