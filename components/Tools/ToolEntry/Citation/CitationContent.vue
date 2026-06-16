4c60b979153fc15580300eb9ff95dd33dc229ba7
avalle <ani.valle@bsc.es>
2026-06-16 17:04:11 +0200

    Fix bug. render HTML in the titles of posts.

--- components/Tools/ToolEntry/Citation/CitationContent.vue
+++ components/Tools/ToolEntry/Citation/CitationContent.vue
@@ -37,11 +37,13 @@
 									:href="'https://doi.org/' + item.term.doi"
 									target="_blank"
 									class="publication-title"
-									>{{ item.term.title }}</a
-								>
-								<span v-else class="publication-title">{{
-									item.term.title
-								}}</span>
+									v-html="item.term.title"
+								></a>
+								<span
+									v-else
+									class="publication-title"
+									v-html="item.term.title"
+								></span>
 
 								<!-- Authors -->
 								<div class="text-body-2 text--secondary mt-1">
